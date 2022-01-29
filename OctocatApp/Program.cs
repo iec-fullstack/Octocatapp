d4284395447a364df8e47ddb8daa067d1a849191
programmer coder <ienjoycoding.fullstack@gmail.com>
2022-01-29 21:28:37 +0530

    sample proj

---
+++ OctocatApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OctocatApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
